03631e8a2cdad2e893b6493d89a731926e246375
adamwasiak <adam.wasiak82@gmail.com>
2021-12-26 19:25:28 +0000

    Correct player welcome message format

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import random
 
+# Welcome message to the player and player name input
 print("Hangman game by Adam Wasiak, welcome!")
 name = input("Enter your name please:")
-print("Hello" + name + "Enjoy the game!")
+print("Hello " + name + "," + " enjoy the game!")
 print("Are you ready?, let's start")
 
