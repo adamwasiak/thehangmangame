0330d0496dc3df9d5a3e696a36a1e1f68e018bc8
adamwasiak <adam.wasiak82@gmail.com>
2021-12-26 18:57:03 +0000

    Add game welcome message and import random

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("Hangman game by Adam Wasiak, welcome!")
+name = input("Enter your name please:")
+print("Hello" + name + "Enjoy the game!")
+print("Are you ready?, let's start")
+
