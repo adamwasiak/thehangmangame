e6c9517a281a30cb5f7c86fec6ddf120eba6827a
adamwasiak <adam.wasiak82@gmail.com>
2021-12-28 21:11:20 +0000

    Correct intendation of the table

--- run.py
+++ run.py
@@ -8,10 +8,10 @@ print("Are you ready?, let's start")
 
 # Add dictonary for words selection
 dictionary = ["year", "dog", "name", "twenty", "car", "february",
-             "window", "computer", "coffee", "hawk", "shopping", "bat",
-             "lane", "tower", "crystal", "gold", "silver", "cat",
-             "impossible", "nice", "game", "sharp", "books", "radio",
-             "speed", "slow", "nothing", "history", "mountain", "lake"]
+              "window", "computer", "coffee", "hawk", "shopping", "bat",
+              "lane", "tower", "crystal", "gold", "silver", "cat",
+              "impossible", "nice", "game", "sharp", "books", "radio",
+              "speed", "slow", "nothing", "history", "mountain", "lake"]
 
 
 def hangman():
