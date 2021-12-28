e5fac6eb7e2b616317ce4b2c68c9531fd3db0815
adamwasiak <adam.wasiak82@gmail.com>
2021-12-28 19:58:56 +0000

    Update hangman logic and add variables to main function

--- run.py
+++ run.py
@@ -24,11 +24,41 @@ def hangman():
     guess = guess.strip()
     if len(guess.strip()) == 0 or len(guess.strip()) == "2" or guess == "9":
         print("Incorrect Input, please use letter instead\n")
+        hangman()
+    
+    elif guess in word:
+        guessed.extend([guess])
+        index = word.find(guess)
+        word = word[:index]+ "_" + word[index+1:]
+        display = display[:index] + guess + display[index+1:]
+        print(display+"\n")
+    elif guess in guessed:
+        print("You have already selected this letter.\n")
+    
+    else:
+        count+=1
+
+        if count==1:
+        print("   _____ \n"
+              "  |      \n"
+              "  |      \n"
+              "  |      \n"
+              "  |      \n"
+              "  |      \n"
+              "  |      \n"
+              "__|__\n")
+        print("Incorrect guess." + str(limit - count) + "guesses left\n")
+
+
         
 
 def replay():    
 
-def main():
+def main(): 
+    count
+    display
+    word
+    guessed
 
 
 
