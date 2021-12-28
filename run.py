9313f6581191b874b3d586c000b176a470fa10f1
adamwasiak <adam.wasiak82@gmail.com>
2021-12-28 18:45:40 +0000

    Add dictonary for game words selection

--- run.py
+++ run.py
@@ -6,6 +6,12 @@ name = input("Enter your name please:")
 print("Hello " + name + "," + " enjoy the game!")
 print("Are you ready?, let's start")
 
+#Add dictonary for words selection
+dictonary= ["year","dog","name","twenty","car","february",
+    "window","computer","coffee","hawk","shopping","bat",
+    "lane","tower","crystal","gold","silver","cat",
+    "impossible","nice","game","sharp","books","radio",
+    "speed","slow","nothing","history","mountain","lake"]
 
 
 def hangman():
